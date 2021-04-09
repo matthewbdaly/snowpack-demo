1b2e27a9ad87dde701fe2c0c6d9fef58f710af22
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2021-04-09 17:29:32 +0100

    Amended ESLint config

--- js/components/App.tsx
+++ js/components/App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
 const App = () => {
-    return (
-        <div>
-        </div>
-    );
+  return (
+    <div>
+    </div>
+  );
 };
 
 export default App;
