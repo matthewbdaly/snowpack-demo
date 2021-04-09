298e6a211f4aafe80da895fb65779f35ea08ee12
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2021-04-09 21:28:11 +0100

    Configured CSSNano

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+const tailwind = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+const plugins =
+  process.env.NODE_ENV === 'production'
+    ? [tailwind, autoprefixer, cssnano]
+    : [tailwind, autoprefixer];
+
+module.exports = { plugins };
